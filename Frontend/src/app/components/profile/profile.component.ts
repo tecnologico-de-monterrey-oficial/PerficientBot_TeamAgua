23693dee7ab702015e741849215cd1469ef70e2e
Jose Miguel <a00227714@tec.mx>
2023-05-20 12:36:32 -0600

    second login alternative

--- Frontend/src/app/components/profile/profile.component.ts
+++ Frontend/src/app/components/profile/profile.component.ts
@@ -1,11 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '@auth0/auth0-angular';
+import {map} from "rxjs";
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit{
+  title = 'Decoded ID Token';
+  code= '';
+
+  user$ = this.auth.user$;
+  code$ = this.user$.pipe(map((user) => JSON.stringify(user, null, 2)));
+
+
 
   constructor(public auth: AuthService) { }
 
