c7057345c8bb3819678949a9c97eccf338196f72
AndresR4601 <75329419+AndresR4601@users.noreply.github.com>
2023-05-21 16:31:52 -0600

    Add meeting url and begin unit testing

---
+++ Backend/pyback/Unit_TestAPI.py
@@ -0,0 +1,19 @@
+import unittest
+from flask import Flask
+from flask.testing import FlaskClient
+from AguaAPI import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_uploadCV(self):
+        response = self.app.post('/upload/123')
+        self.assertEqual(response.status_code, 200)
+
+    def test_getGithubRepos(self):
+        response = self.app.get('/Github/Repositories')
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
