d1cc22c75a1e00b91cbcac66a67b7f3b0c8d6514
AndresR4601 <75329419+AndresR4601@users.noreply.github.com>
2023-05-24 00:20:36 -0600

    Update Unit_TestAPI.py

--- Backend/pyback/Unit_TestAPI.py
+++ Backend/pyback/Unit_TestAPI.py
@@ -8,17 +8,54 @@ class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_uploadCV(self):
-        response = self.app.post('/upload/123')
+    def test_Happy_PathUploadCV(self):
+        response = self.app.post('/upload/windowslive|f6a8eb57361df774')
         self.assertEqual(response.status_code, 200)
 
-    def test_getGithubRepos(self):
+    def test_Happy_PathGetSummary(self):
+        response = self.app.get('/CV/windowslive|f6a8eb57361df774')
+        self.assertEqual(response.status_code, 200)
+
+    def test_Happy_PathGetCV(self):
+        response = self.app.get('/GPTtext/windowslive|f6a8eb57361df774')
+        self.assertEqual(response.status_code, 200)
+
+    def test_Happy_PathGetGithubRepos(self):
         response = self.app.get('/Github/Repositories')
         self.assertEqual(response.status_code, 200)
         
-    def test_getGithubIssues(self):
+    def test_Happy_PathGetGithubIssues(self):
         response = self.app.get('/Github/Issues')
         self.assertEqual(response.status_code, 200)
+        
+    def test_Happy_PathGetGithubPulls(self):
+        response = self.app.get('/Github/Pulls')
+        self.assertEqual(response.status_code, 200)
+
+    def test_Happy_PathGetOutlookGroups(self):
+        response = self.app.get('/Outlook/Groups')
+        self.assertEqual(response.status_code, 200)
+
+    def test_postOutlookFindTime(self):
+        with app.test_client() as client:
+            input_data = {
+            "emailAddress": {"address": "A00831316@tec.mx"},
+            "startDateTime": "2023-05-24T09:00:00",
+            "finishDateTime": "2023-05-26T09:00:00",
+            "duration": "PT1H"
+            }
+        response = client.post('/Outlook/FindTime', json=input_data)
+        assert response.status_code == 200
+
+    def test_postCreateItem(self):
+        with app.test_client() as client:
+            input_data = {
+            "title": "Listo",
+            "description": "Se logro",
+            "type": "Task"
+            }
+        response = client.post('/Azure/CreateItem', json=input_data)
+        assert response.status_code == 200
 
 if __name__ == '__main__':
     unittest.main()
