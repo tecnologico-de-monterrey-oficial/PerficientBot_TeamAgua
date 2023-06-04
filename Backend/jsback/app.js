2a4c2c52957716bdb2a117cc8c29eb4ddaee4fdd
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2023-06-03 21:30:34 -0600

    Update app.js

--- Backend/jsback/app.js
+++ Backend/jsback/app.js
@@ -8,9 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 // Initial configuration
-const app = express();
-app.use(express.json());
-app.use(cors());
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('login', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
