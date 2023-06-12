db1d074fa4c86f9da6c98653ab98870df2a80670
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2023-06-12 11:21:49 -0600

    Update conversation.js

--- Backend/jsback/controllers/conversation.js
+++ Backend/jsback/controllers/conversation.js
@@ -2,10 +2,12 @@ const { generateNewToken } = require('../functions/token');
 
 const conversation = async(req, res) => {
   	// check header or url parameters or post parameters for token
-   res.setHeader('Access-Control-Allow-Origin', '*');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    res.set('Access-Control-Allow-Origin', '*');
+
+  //  res.setHeader('Access-Control-Allow-Origin', '*');
+  //  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  //  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+  //   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
     const { user_message, secret_key } = req.body;
 
