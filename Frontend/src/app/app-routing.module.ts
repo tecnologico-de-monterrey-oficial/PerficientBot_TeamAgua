e8aa7b94d4f9ef09962578ed1477de1b4effe5ad
Eunice Santos <eunisantos218@gmail.com>
2023-06-13 02:17:30 -0600

    rutas protegidas

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent },
-  // { path: '**', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent}
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard], }, //protected routes by auth
+  // { path: '**', redirectTo: 'login', pathMatch: 'full' }, //protected routes by auth
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard],}
 ];
 
 @NgModule({
