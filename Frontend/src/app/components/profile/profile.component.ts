c19263a6f4c4a4a48087013396b603c2c2034b0a
Eunice Santos <eunisantos218@gmail.com>
2023-06-13 02:43:56 -0600

    refactor fetchhr, update protecting routes method

--- Frontend/src/app/components/profile/profile.component.ts
+++ Frontend/src/app/components/profile/profile.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from '@auth0/auth0-angular';
 import {map} from "rxjs";
 import { HttpClient } from '@angular/common/http';
 import { filter } from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -26,27 +27,33 @@ export class ProfileComponent implements OnInit{
   user_email!: string;
   user_id!: string; //Sub
 
-  constructor(private http: HttpClient, public auth: AuthService) { }
+  constructor(private http: HttpClient, public auth: AuthService, private router: Router) { }
 
 
-  ngOnInit(): void {
-    this.fetchIsHR();
-    this.auth.user$.subscribe(user => {
+  ngOnInit(): void { 
+    this.auth.isAuthenticated$.subscribe(isAuthenticated => {
+      if(!isAuthenticated){
+        //protects route using auth service and route module
+        this.router.navigate(['/login']).then(r => console.log(r));
+      }
+
+      this.auth.user$.subscribe(user => {
       if (user) {
         this.user_givenname = user.given_name || '';
         this.user_familyname = user.family_name || '';
         this.user_name = user.name || '';
         this.user_email = user.email || '';
         this.user_id = user.sub || '';
-
+        this.fetchIsHR(this.user_id);
         this.postDataToDatabase();
       }
     });
+
+    })
     
   }
 
   
-
 
   postDataToDatabase() {
     // This is your data that you want to send in POST request
@@ -70,13 +77,10 @@ export class ProfileComponent implements OnInit{
   }
 
   //validation for HR authorized 
-  fetchIsHR(): void {
-    this.auth.user$
-      // @ts-ignore
-      .pipe(filter(user => user !== null && user.sub !== null))
-      .subscribe(user => {
-        // @ts-ignore
-        const userId = user.sub.replace('|', '_');  // replace | with _ in user ID
+  fetchIsHR(userSub: string): void {
+    //it takes userSub parameter obtain in ngOnInit
+    //deleted validation since is already done in OnInit method
+    const userId = userSub.replace('|', '_');  // replace | with _ in user ID
         this.http.get(`http://localhost:3001/api/CheckHR`, { params: { sub: userId } }).subscribe((response: any) => {
 
           if (response.length > 0) {
@@ -84,7 +88,6 @@ export class ProfileComponent implements OnInit{
             this.isHR = response[0].IsHR;
           }
         });
-      });
   }
 
 
